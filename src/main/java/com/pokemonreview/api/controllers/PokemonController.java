998608e8b4977c9101b5fe5b0f430e14f3c91683
Abdi Abdulle <cph-aa394@cphbusiness.dk>
2023-07-16 21:24:35 +0200

    controller methods created + a repository interface

--- src/main/java/com/pokemonreview/api/controllers/PokemonController.java
+++ src/main/java/com/pokemonreview/api/controllers/PokemonController.java
@@ -2,10 +2,9 @@ package com.pokemonreview.api.controllers;
 
 
 import com.pokemonreview.api.models.PokemonEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,5 +21,33 @@ public class PokemonController {
         pokemons.add(new PokemonEntity(3, "Charmander", "Fire"));
         return ResponseEntity.ok(pokemons);
     }
+
+    @GetMapping("pokemon/{id}")
+    public PokemonEntity pokemonGetId(@PathVariable int id) {
+        return new PokemonEntity(id, "Squirtle", "Water");
+    }
+
+    @PostMapping("pokemon/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<PokemonEntity> createPokemon(@RequestBody PokemonEntity pokemon){
+        System.out.println(pokemon.getName());
+        System.out.println(pokemon.getType());
+        return new ResponseEntity<>(pokemon, HttpStatus.CREATED);
+    }
+
+    @PutMapping("pokemon/{id}/update")
+    public ResponseEntity<PokemonEntity> updatePokemon(@RequestBody PokemonEntity pokemon,
+                                                       @PathVariable("id") int pokemonId){
+        System.out.println(pokemon.getName());
+        System.out.println(pokemon.getType());
+        return ResponseEntity.ok(pokemon);
+    }
+
+    @DeleteMapping("pokemon/{id}/delete")
+    public ResponseEntity<String> deletePokemon(@PathVariable("id") int pokemonId){
+        System.out.println(pokemonId);
+        return ResponseEntity.ok("Pokemon deleted successfully");
+    }
+
 
 }
